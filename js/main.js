611194e823ab3cc00baea1e3544f8e00346089e5
FRUHD <99921514+FRUHD@users.noreply.github.com>
2022-04-13 23:19:05 -0700

    spell fix. hide and show images. add local storage. add shuffle.

--- js/main.js
+++ js/main.js
@@ -1,24 +1,45 @@
 let deckID = ''   // Global variable
+console.log(localStorage.getItem('myDeck'))
 
-fetch('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1')  // Grab a deck
+if(!localStorage.getItem('myDeck')) {
+  fetch('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1')  // Grab a deck
       .then(res => res.json()) // parse response as JSON
       .then(data => {
         console.log(data)
         deckID = data.deck_id  // Store Deck ID in variable
+
+        localStorage.setItem('myDeck', deckID)
       })
       .catch(err => {
           console.log(`error ${err}`)
       });
+} else {
+  deckID = localStorage.getItem('myDeck')
+  console.log('we have ID')
+}
 
 document.getElementById('btn').addEventListener('click', drawTwo)
 
 function drawTwo(){
+
+  document.querySelector('#player1War1').style.display = 'none'
+  document.querySelector('#player1War2').style.display = 'none'
+  document.querySelector('#player1War3').style.display = 'none'
+  document.querySelector('#player2War1').style.display = 'none'
+  document.querySelector('#player2War2').style.display = 'none'
+  document.querySelector('#player2War3').style.display = 'none'
+
   const url = `https://deckofcardsapi.com/api/deck/${deckID}/draw/?count=2`  // Grabbin' two cards
 
   fetch(url)
       .then(res => res.json()) // parse response as JSON
       .then(data => {
         console.log(data)
+        if (!data.success) {
+          fetch(`https://deckofcardsapi.com/api/deck/${deckID}/shuffle/`)
+          .then(drawTwo())
+          return
+        }
         document.querySelector('#player1').src = data.cards[0].image
         document.querySelector('#player2').src = data.cards[1].image
         let player1Valoo = convertToNum(data.cards[0].value)
@@ -30,8 +51,9 @@ function drawTwo(){
         } else {
           document.querySelector('h3').innerText = 'This. Is. WAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAARRRR!!!!!!!!!!!!'
           
-          document.getElementById('btn').innterText = "WAR!!!"  // Change the button to WAR
-          document.getElementById('btn').addEventListener('click', drawWar) // Draw 8 cards        
+          document.getElementById('btn').innerText = "WAR!!!"  // Change the button to WAR
+          document.getElementById('btn').removeEventListener('click', drawTwo)
+          document.getElementById('btn').addEventListener('click', drawWar) // Draw 8 cards
         }
 
       })
@@ -55,11 +77,23 @@ function convertToNum(val) {
 
 function drawWar(){
   const urlWar = `https://deckofcardsapi.com/api/deck/${deckID}/draw/?count=8`  // Grabbin' eight cards
+  
+  document.querySelector('#player1War1').style.display = ''
+  document.querySelector('#player1War2').style.display = ''
+  document.querySelector('#player1War3').style.display = ''
+  document.querySelector('#player2War1').style.display = ''
+  document.querySelector('#player2War2').style.display = ''
+  document.querySelector('#player2War3').style.display = ''
 
   fetch(urlWar)
       .then(res => res.json()) // parse response as JSON
       .then(data => {
         console.log(data)
+        if (!data.success) {
+          fetch(`https://deckofcardsapi.com/api/deck/${deckID}/shuffle/`)
+          .then(drawWar())
+          return
+        }
         document.querySelector('#player1').src = data.cards[3].image
           document.querySelector('#player1War1').src = data.cards[0].image
           document.querySelector('#player1War2').src = data.cards[1].image
@@ -70,21 +104,23 @@ function drawWar(){
           document.querySelector('#player2War3').src = data.cards[6].image
         let player1Valoo = convertToNum(data.cards[3].value)
         let player2Valoo = convertToNum(data.cards[7].value)
-        if (player1Valoo > player2Valoo) {
-          document.querySelector('h3').innerText = 'PLAYER 1 HAS CONQUERED!'
-        } else if (player1Valoo < player2Valoo) {
-          document.querySelector('h3').innerText = 'PLAYER 2 PREVAILS!'
-        } else {
+        if (player1Valoo === player2Valoo) {
           document.querySelector('h3').innerText = 'This. Is. WAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAARRRR!!!!!!!!!!!! (again)'
-          
-          document.getElementById('btn').innterText = "WAR!!!"  // Change the button to WAR
-          document.getElementById('btn').addEventListener('click', drawWar) // Draw 8 cards   
+        } else {
+          // Change the button back
+          document.getElementById('btn').innerText = "Deal Two Cards!"
+          document.getElementById('btn').removeEventListener('click', drawWar)
+          document.getElementById('btn').addEventListener('click', drawTwo)
+
+          if (player1Valoo > player2Valoo) {
+            document.querySelector('h3').innerText = 'PLAYER 1 HAS CONQUERED!'
+          } else {
+            document.querySelector('h3').innerText = 'PLAYER 2 PREVAILS!'
+          }
         }
-        // Change the button back
       })
       .catch(err => {
           console.log(`error ${err}`)
       });
 }
 
-// Store deck to local storage
